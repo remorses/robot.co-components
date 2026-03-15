7799558a16c2e35309e03284f34188fa1851694c
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-03-15 22:55:33 +0100

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
 // Spiceflow app entry point for robot.co-components demo
-import './globals.css'
-import { Spiceflow } from 'spiceflow'
-import NodeGrid from './nodegrid/page'
+import "./globals.css";
+import { Spiceflow } from "spiceflow";
+import NodeGrid from "./nodegrid/page";
 
-const app = new Spiceflow()
-  .layout('/*', async ({ children }) => {
+export const app = new Spiceflow()
+  .layout("/*", async ({ children }) => {
     return (
       <html lang="en">
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
-    )
+    );
   })
-  .page('/', async () => {
-    return <NodeGrid />
-  })
+  .page("/", async () => {
+    return <NodeGrid />;
+  });
 
-export default app
+app.listen(3000);
+
+export default app;
